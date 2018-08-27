dfb8787268a44aa9507be3373aa329e6f39b503c
Praveen <selvaprvn@gmail.com>
2018-08-27 17:05:34 +0530

    examples

--- lib/grid.ts
+++ lib/grid.ts
@@ -1,3 +1,5 @@
+import { Column } from "./interfaces/column";
+
 /**
  * @Method: Returns the test msg.
  * @Param {string}
@@ -5,4 +7,25 @@
  */
 export function getTest(str: any): string {
   return `Test : ${str}`;
+}
+
+export class DataGrid {
+  columns: Column[] = [];
+
+  private virtualElement: HTMLDivElement;
+  constructor() {
+    this.virtualElement = document.createElement("div");
+  }
+
+  virtualInit() {
+    this.init(this.virtualElement);
+  }
+
+  init(element: HTMLElement) {
+    if (!element) {
+      throw new Error("element to render should be provoided");
+    }
+    console.log(element);
+    
+  }
 }
