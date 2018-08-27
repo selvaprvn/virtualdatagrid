58625e51053cb5f5b658fa946392cf77919f4136
Praveen <selvaprvn@gmail.com>
2018-08-27 14:58:49 +0530

    travis

---
+++ lib/grid.ts
@@ -0,0 +1,8 @@
+/**
+ * @Method: Returns the test msg.
+ * @Param {string}
+ * @Return {string}
+ */
+export function getTest(str: any): string {
+  return `Test : ${str}`;
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,3 @@
-/**
- * @Method: Returns the test msg.
- * @Param {string}
- * @Return {string}
- */
-export function getTest(str: any): string {
-  return `Test : ${str}`;
-}
+import { getTest } from "./grid";
+
+export { getTest };
